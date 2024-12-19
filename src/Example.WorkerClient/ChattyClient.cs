d19f644d2237d2e447745e0a5ff27e95e73a1277
Saar Shen <saars@microsoft.com>
2024-12-19 13:00:30 -0800

    Append a chatty client that talks to the server every 5 seconds

---
+++ src/Example.WorkerClient/ChattyClient.cs
@@ -0,0 +1,52 @@
+using CodeWithSaar.IPC;
+using Example.DataContracts;
+
+namespace Example.WorkerClient;
+
+internal class ChattyClient : BackgroundService
+{
+    private readonly NamedPipeClientFactory _namedPipeClientFactory;
+    private readonly ILogger<ChattyClient> _logger;
+
+    public ChattyClient(
+        NamedPipeClientFactory namedPipeClientFactory,
+        ILogger<ChattyClient> logger)
+    {
+        _namedPipeClientFactory = namedPipeClientFactory ?? throw new ArgumentNullException(nameof(namedPipeClientFactory));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            using (INamedPipeClientService namedPipeClient = _namedPipeClientFactory.CreateClient())
+            {
+                _logger.LogInformation("[CLIENT] Connecting to named pipe server...");
+                await namedPipeClient.ConnectAsync(TimeSpan.FromSeconds(10), stoppingToken).ConfigureAwait(false);
+                _logger.LogInformation("[CLIENT] Connected to named pipe server.");
+
+                string whatTheServerSay = await namedPipeClient.ReadMessageAsync().ConfigureAwait(false);
+                _logger.LogInformation("[CLIENT] The server says: {content}", whatTheServerSay);
+                whatTheServerSay = await namedPipeClient.ReadMessageAsync().ConfigureAwait(false);
+                _logger.LogInformation("[CLIENT] The server says(2): {content}", whatTheServerSay);
+
+                string reply = "Hey from the client";
+                _logger.LogInformation("[CLIENT] Telling the server: {content}", reply);
+                await namedPipeClient.SendMessageAsync(reply);
+                Customer serverCustomer = await namedPipeClient.ReadAsync<Customer>().ConfigureAwait(false);
+                _logger.LogInformation("[CLIENT] Server customer name: {content}", serverCustomer.Name);
+
+                // Send objects back and forth
+                Customer clientCustomer = new()
+                {
+                    Name = "Client Customer",
+                };
+                await namedPipeClient.SendAsync(clientCustomer).ConfigureAwait(false);
+            }
+
+            // Wait for 5 sends before chat with the server again
+            await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken).ConfigureAwait(false);
+        }
+    }
+}
